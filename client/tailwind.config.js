6b58e1e604eadcb75555890b134f6477bc4ed1ef
jack mcqueen <jdmcqueen44@gmail.com>
2023-07-07 13:29:18 -0400

    General & landing page config

---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      logoBg: "#2F2F2F",
+      main: "#282828",
+      gold: "#FDC959",
+      green: "#3AB454",
+      txt1: "#FFFFFF",
+      txt2: "#CECECE",
+    },
+    fontFamily: {
+      logotext: ["Zen Kurenaido", "sans-serif"],
+      subtext: ["Amiri", "serif"],
+      headingtext: ["Marcellus", "serif"],
+    },
+    extend: {
+      gridTemplateColumns: {
+        features: "40% 30%",
+      },
+      boxShadow: {
+        primary: "4px 4px 9px rgba(0,0,0, 0.25)",
+      },
+    },
+  },
+  plugins: [],
+};
