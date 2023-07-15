32951e60e23758a202667bdfb56719065ebd2989
jack mcqueen <jdmcqueen44@gmail.com>
2023-07-15 16:13:43 -0400

    Alter action icons

--- client/src/assets/icons/export.ts
+++ client/src/assets/icons/export.ts
@@ -1,17 +1,23 @@
 import add from "./add.svg";
 import build from "./build.svg";
 import search from "./search.svg";
+import edit from "./edit.svg";
+import del from "./delete.svg";
 
 interface Icons {
   add: string;
   build: string;
   search: string;
+  edit: string;
+  del: string;
 }
 
 const icons: Icons = {
   add,
   build,
   search,
+  edit,
+  del,
 };
 
 export default icons;
