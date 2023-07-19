aa5f8aee635e9cac871f84c25c193efd7853773d
jack mcqueen <jdmcqueen44@gmail.com>
2023-07-19 15:53:29 -0400

    Add dashboard nav icons

--- client/src/assets/icons/export.ts
+++ client/src/assets/icons/export.ts
@@ -5,6 +5,10 @@ import edit from "./edit.svg";
 import del from "./delete.svg";
 import back from "./back.svg";
 import close from "./close.svg";
+import settings from "./settings.svg";
+import logout from "./logout.svg";
+import menu from "./menu.svg";
+import recipe from "./recipe.svg";
 
 interface Icons {
   add: string;
@@ -14,6 +18,10 @@ interface Icons {
   del: string;
   back: string;
   close: string;
+  settings: string;
+  logout: string;
+  menu: string;
+  recipe: string;
 }
 
 const icons: Icons = {
@@ -24,6 +32,10 @@ const icons: Icons = {
   del,
   back,
   close,
+  settings,
+  logout,
+  menu,
+  recipe,
 };
 
 export default icons;
