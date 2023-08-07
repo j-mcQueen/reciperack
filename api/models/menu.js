e14e063c47b3a0d88716d057fa3e0c8818c0d3ba
jack mcqueen <jdmcqueen44@gmail.com>
2023-08-07 15:46:41 -0400

    Update: extend day object in Menu Schema

--- api/models/menu.js
+++ api/models/menu.js
@@ -6,12 +6,24 @@ const MenuSchema = new Schema({
   monday: [
     { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
   ],
-  tuesday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  wednesday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  thursday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  friday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  saturday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
-  sunday: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+  tuesday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
+  wednesday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
+  thursday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
+  friday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
+  saturday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
+  sunday: [
+    { meal: Number, recipe: { type: Schema.Types.ObjectId, ref: "Recipe" } },
+  ],
 });
 
 module.exports = mongoose.model("Menu", MenuSchema);
