6133d33b422893cd4d94298128837dafc480793e
jack mcqueen <jdmcqueen44@gmail.com>
2023-08-07 16:56:11 -0400

    Fix: bug causing incorrect menu recipes to delete

--- client/src/components/dashboard/menus/Table.tsx
+++ client/src/components/dashboard/menus/Table.tsx
@@ -1,56 +1,84 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import TableRow from "./TableRow";
+import DeleteRecipe from "../recipes/DeleteRecipe";
 
 export default function Table({ ...props }) {
-  const day = props.activeDay.toLowerCase();
-  const dayRecipes = props.menu[day];
+  const [breakfast, setBreakfast] = useState({});
+  const [lunch, setLunch] = useState({});
+  const [dinner, setDinner] = useState({});
 
   useEffect(() => {
+    setBreakfast({});
+    setLunch({});
+    setDinner({});
+  }, [props.activeDay]);
+
+  useEffect(() => {
+    const day = props.activeDay.toLowerCase();
+    const dayRecipes = props.menu[day];
+
     for (const recipe of dayRecipes) {
       if (recipe.meal === 0) {
-        props.setBreakfast(recipe);
+        setBreakfast(recipe);
       } else if (recipe.meal === 1) {
-        props.setLunch(recipe);
+        setLunch(recipe);
       } else {
-        props.setDinner(recipe);
+        setDinner(recipe);
       }
     }
-  }, [dayRecipes, props.menu]);
+  }, [props.activeDay, props.menu]);
 
   return (
-    <table className="font-manrope table-fixed border-collapse m-10">
-      <caption className="text-3xl tracking-tighter p-3 border border-solid border-offmain">
-        {props.activeDay} recipes
-      </caption>
+    <>
+      {props.deleteMenuRecipeActive ? (
+        <div className="fixed flex items-center justify-center w-screen h-screen backdrop-brightness-50">
+          <DeleteRecipe
+            setBreakfast={setBreakfast}
+            setLunch={setLunch}
+            setDinner={setDinner}
+            activeDay={props.activeDay}
+            activeMeal={props.activeMeal}
+            menu={props.menu}
+            setMenu={props.setMenu}
+            setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
+            source="menu"
+          />
+        </div>
+      ) : null}
+      <table className="font-manrope table-fixed border-collapse m-10">
+        <caption className="text-3xl tracking-tighter p-3 border border-solid border-offmain">
+          {props.activeDay} recipes
+        </caption>
 
-      <tbody>
-        <TableRow
-          meal={"Breakfast"}
-          recipe={props.breakfast}
-          setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
-          activeDay={props.activeDay}
-          setAddModal={props.setAddModal}
-          setActiveMeal={props.setActiveMeal}
-        />
+        <tbody>
+          <TableRow
+            meal={"Breakfast"}
+            recipe={breakfast}
+            setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
+            activeDay={props.activeDay}
+            setAddModal={props.setAddModal}
+            setActiveMeal={props.setActiveMeal}
+          />
 
-        <TableRow
-          meal={"Lunch"}
-          recipe={props.lunch}
-          setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
-          activeDay={props.activeDay}
-          setAddModal={props.setAddModal}
-          setActiveMeal={props.setActiveMeal}
-        />
+          <TableRow
+            meal={"Lunch"}
+            recipe={lunch}
+            setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
+            activeDay={props.activeDay}
+            setAddModal={props.setAddModal}
+            setActiveMeal={props.setActiveMeal}
+          />
 
-        <TableRow
-          meal={"Dinner"}
-          recipe={props.dinner}
-          setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
-          activeDay={props.activeDay}
-          setAddModal={props.setAddModal}
-          setActiveMeal={props.setActiveMeal}
-        />
-      </tbody>
-    </table>
+          <TableRow
+            meal={"Dinner"}
+            recipe={dinner}
+            setDeleteMenuRecipeActive={props.setDeleteMenuRecipeActive}
+            activeDay={props.activeDay}
+            setAddModal={props.setAddModal}
+            setActiveMeal={props.setActiveMeal}
+          />
+        </tbody>
+      </table>
+    </>
   );
 }
